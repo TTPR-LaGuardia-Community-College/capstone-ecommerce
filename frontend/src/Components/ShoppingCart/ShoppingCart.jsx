fde36f880621682cfc559f68e534cce4fada0fc9
Carlos A. Soler <CARLOSSOLERG319@GMAIL.COM>
2025-06-03 19:45:06 -0400

    i did some changes, i create some components

---
+++ frontend/src/Components/ShoppingCart/ShoppingCart.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavigationBar from '../NavigationBar/NavigationBar'
+import CartElements from './ShoppingCartElements'
+import './ShoppingCart.css'
+
+const ShoppingCart = () => {
+  return (
+    <>
+    <NavigationBar/>
+    <CartElements/>
+    
+    </>
+  )
+}
+
+export default ShoppingCart
