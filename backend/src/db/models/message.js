cd333eeb07f6d1535eb63f4e99d31008147efda5
Nafiz Imtiaz <nafizimtiaz@Nafizs-MacBook-Air.local>
2025-06-12 18:07:57 -0400

    configured messages.js

---
+++ backend/src/db/models/message.js
@@ -0,0 +1,47 @@
+"use strict";
+const { Model } = require("sequelize");
+
+module.exports = (sequelize, DataTypes) => {
+  class Message extends Model {
+    static associate(models) {
+      Message.belongsTo(models.User, { foreignKey: "senderId", as: "sender" });
+
+      Message.belongsTo(models.User, { foreignKey: "receiverId", as: "receiver" });
+    }
+  }
+
+  Message.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      senderId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      receiverId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Message",
+      tableName: "Messages",
+      updatedAt: false,
+    }
+  );
+
+  return Message;
+};
