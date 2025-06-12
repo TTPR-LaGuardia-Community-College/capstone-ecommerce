21ea9f6f8033d80618ae203eb222fd2d53e38951
Nafiz Imtiaz <nafizimtiaz@Nafizs-MacBook-Air.local>
2025-06-12 16:48:31 -0400

    Migration for wishlist done

--- backend/src/db/migrations/20250612203730-create-wishlists.js
+++ backend/src/db/migrations/20250612203730-create-wishlists.js
@@ -1,22 +1,34 @@
-'use strict';
-
-/** @type {import('sequelize-cli').Migration} */
+"use strict";
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Wishlists", {
+      id: {
+        type: Sequelize.UUID,
+        defaultValue: Sequelize.UUIDV4,
+        primaryKey: true,
+      },
+      userId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: { model: "Users", key: "id" },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      listingId: {
+        type: Sequelize.UUID,
+        allowNull: false,
+        references: { model: "Listings", key: "id" },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE",
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        defaultValue: Sequelize.fn("NOW"),
+      },
+    });
   },
-
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
-  }
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Wishlists");
+  },
 };
