e6f9021a463a57386b36a60a0387ac41bb11837a
Nafiz Imtiaz <nafizimtiaz@Nafizs-MacBook-Air.local>
2025-06-12 21:27:58 -0400

    Login.jsx prompt added

---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,14 @@
+// frontend/src/pages/Login.jsx
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import api from "../api.js";
+
+export default function Login() {
+  // need code here!!
+
+  return (
+    <div>
+        // here too!!
+    </div>
+  );
+}
