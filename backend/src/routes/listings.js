c2a945cf6c609f0e5cdcd2348d1db356095a3152
Nafiz Imtiaz <nafizimtiaz@Nafizs-MacBook-Air.local>
2025-06-17 18:22:34 -0400

    fixed bugs in backend

--- backend/src/routes/listings.js
+++ backend/src/routes/listings.js
@@ -1,11 +1,10 @@
 var express = require("express");
-const db = require("../db"); // Ensure this path is correct
+const db = require("../db");
 
 var router = express.Router();
 
-const User = db["User"]; // Assuming you have a User model defined in your models/index.js
+const User = db["User"];
 
-/* GET users listing. */
 router.get("/", function (req, res, next) {
   User.findAll().then((users) => {
     res.json(users);
